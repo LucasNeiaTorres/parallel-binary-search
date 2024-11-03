aee9c9bbea9ed89e6166bbf692de1f1cc9d14761
Lucas Néia <lucasneia07@gmail.com>
2024-11-03 19:27:28 -0300

    insere numThreads linha de comando

--- bsearchA.c
+++ bsearchA.c
@@ -2,10 +2,11 @@
 #include <pthread.h>
 #include <stdlib.h>
 
-#define NUM_THREADS 8
+#define MAX_THREADS 8
 #define MAX_TASKS 16
 
-typedef struct {
+typedef struct
+{
     long long *input;
     int left;
     int right;
@@ -14,7 +15,7 @@ typedef struct {
     int id;
 } ThreadData;
 
-pthread_t threads[NUM_THREADS];
+pthread_t threads[MAX_THREADS];
 ThreadData taskQueue[MAX_TASKS];
 int taskIndex = 0;
 int tasksInQueue = 0;
@@ -25,22 +26,31 @@ pthread_mutex_t completeMutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t queueCond = PTHREAD_COND_INITIALIZER;
 pthread_cond_t completeCond = PTHREAD_COND_INITIALIZER;
 
+int numThreads;
+
 // Implementação de busca binária (bsearch_lower_bound)
-int bsearch_lower_bound(long long *input, int left, int right, long long x) {
-    while (left < right) {
+int bsearch_lower_bound(long long *input, int left, int right, long long x)
+{
+    while (left < right)
+    {
         int m = left + (right - left) / 2;
-        if (input[m] < x) left = m + 1;
-        else right = m;
+        if (input[m] < x)
+            left = m + 1;
+        else
+            right = m;
     }
     return left;
 }
 
 // Função de trabalho da thread
-void *thread_worker(void *arg) {
-    while (1) {
+void *thread_worker(void *arg)
+{
+    while (1)
+    {
         pthread_mutex_lock(&queueMutex);
 
-        while (tasksInQueue == 0) pthread_cond_wait(&queueCond, &queueMutex);
+        while (tasksInQueue == 0)
+            pthread_cond_wait(&queueCond, &queueMutex);
 
         // Pega uma tarefa da fila
         ThreadData task = taskQueue[taskIndex];
@@ -56,7 +66,8 @@ void *thread_worker(void *arg) {
         // Marca a tarefa como concluída
         pthread_mutex_lock(&completeMutex);
         completedTasks++;
-        if (completedTasks == NUM_THREADS) {
+        if (completedTasks == numThreads)
+        {
             pthread_cond_signal(&completeCond);
         }
         pthread_mutex_unlock(&completeMutex);
@@ -64,14 +75,17 @@ void *thread_worker(void *arg) {
 }
 
 // Inicializa o pool de threads
-void init_thread_pool() {
-    for (int i = 0; i < NUM_THREADS; i++) {
+void init_thread_pool()
+{
+    for (int i = 0; i < numThreads; i++)
+    {
         pthread_create(&threads[i], NULL, thread_worker, NULL);
     }
 }
 
 // Adiciona uma tarefa ao pool de threads
-void add_task(long long *input, int left, int right, long long target, int *result) {
+void add_task(long long *input, int left, int right, long long target, int *result)
+{
     pthread_mutex_lock(&queueMutex);
 
     taskQueue[(taskIndex + tasksInQueue) % MAX_TASKS] = (ThreadData){input, left, right, target, result, -1};
@@ -82,15 +96,17 @@ void add_task(long long *input, int left, int right, long long target, int *resu
 }
 
 // Função de busca paralela com espera pelas threads
-int parallel_bsearch_lower_bound(long long *input, int n, long long x) {
-    ThreadData threadData[NUM_THREADS];
+int parallel_bsearch_lower_bound(long long *input, int n, long long x)
+{
+    ThreadData threadData[numThreads];
     completedTasks = 0;
 
     // Cria as tarefas para cada thread
-    for (int i = 0; i < NUM_THREADS; i++) {
+    for (int i = 0; i < numThreads; i++)
+    {
         threadData[i].input = input;
-        threadData[i].left = i * n / NUM_THREADS;
-        threadData[i].right = (i + 1) * n / NUM_THREADS;
+        threadData[i].left = i * n / numThreads;
+        threadData[i].right = (i + 1) * n / numThreads;
         threadData[i].target = x;
         threadData[i].result = malloc(sizeof(int));
         *threadData[i].result = n;
@@ -99,16 +115,19 @@ int parallel_bsearch_lower_bound(long long *input, int n, long long x) {
 
     // Aguarda até que todas as tarefas estejam concluídas
     pthread_mutex_lock(&completeMutex);
-    while (completedTasks < NUM_THREADS) {
+    while (completedTasks < numThreads)
+    {
         pthread_cond_wait(&completeCond, &completeMutex);
     }
     pthread_mutex_unlock(&completeMutex);
 
     // Calcula o menor índice resultante
     int result = n;
-    for (int i = 0; i < NUM_THREADS; i++) {
+    for (int i = 0; i < numThreads; i++)
+    {
         // printf("Thread %d: %d\n", i, *threadData[i].result);
-        if ((*threadData[i].result < n) && (input[*threadData[i].result] >= x)) {
+        if ((*threadData[i].result < n) && (input[*threadData[i].result] >= x))
+        {
             result = *threadData[i].result < result ? *threadData[i].result : result;
         }
         free(threadData[i].result);
@@ -118,27 +137,43 @@ int parallel_bsearch_lower_bound(long long *input, int n, long long x) {
 }
 
 // Funções auxiliares de geração e ordenação
-void geraNaleatorios(long long v[], int n) {
-    for (int i = 0; i < n; i++) {
+void geraNaleatorios(long long v[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
         v[i] = rand() % 1000;
     }
 }
 
-int compare(const void *a, const void *b) {
+int compare(const void *a, const void *b)
+{
     return (*(long long *)a - *(long long *)b);
 }
 
-void printVetor(long long v[], int n) {
-    for (int i = 0; i < n; i++) {
+void printVetor(long long v[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
         printf("%lld ", v[i]);
     }
     printf("\n");
 }
 
-int main() {
+int main(int argc, char *argv[])
+{
     int n = 16000000;
     long long *input = malloc(n * sizeof(long long));
     long long target = 755;
+
+    if (argc != 2) {
+        printf("usage: %s <nThreads>\n", argv[0]);
+        return 0;
+    } else if(atoi(argv[1]) > MAX_THREADS) {
+        printf("Número de threads maior que o máximo permitido\n");
+        return 0;
+    } else {
+        numThreads = atoi(argv[1]);
+    }
 
     srand(time(NULL));
 
